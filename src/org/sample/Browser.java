aa0a6988eb20253525207b13d7487e85c3ce4f27
aniparu <aniparu122@gmail.com>
2023-02-28 19:20:18 +0530

    Done

---
+++ src/org/sample/Browser.java
@@ -0,0 +1,26 @@
+package org.sample;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Browser {
+	public static void main(String[] args ) throws AWTException, InterruptedException {
+		System.out.println("Arafa Git");
+		
+		
+	
+        }   
+	
+	}
+
+
